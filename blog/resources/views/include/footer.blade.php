9606a76be47535b9b3e2c337bf882f4861d9dd3b
EvSha83 <EvSha83@yandex.ru>
2021-10-03 17:09:16 +0300

    add main page view

---
+++ blog/resources/views/include/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="py-3 my-4 fixed-bottom border-top">
+    <p class="text-center text-muted">© {{date('Y')}} Test Laravel Blog</p>
+</footer>
---
+++ blog/resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main-layout')
+
+@section('title','My Blog')
+
+@section('content')
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cum doloribus error eveniet maxime mollitia nobis odio repellat! Architecto cupiditate deleniti dolor eaque fuga magnam quo quos veniam. Eligendi.</p>
+
+@endsection
+
+
+
