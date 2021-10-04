d71b9fd600eef0b3c6ad5beaa4117777058b7aa7
EvSha83 <EvSha83@yandex.ru>
2021-10-04 17:45:30 +0300

    feat:migration comments table and fix comment_form

--- blog/resources/views/include/comment_form.blade.php
+++ blog/resources/views/include/comment_form.blade.php
@@ -1,11 +1,11 @@
 <form action="#" method="post">
     <div class="mb-3">
         <label for="username" class="form-label">User Name</label>
-        <input type="text" name="name" class="form-control" id="username" placeholder="John">
+        <input type="text" name="username" class="form-control" id="username" placeholder="John">
     </div>
     <div class="mb-3">
         <label for="comment" class="form-label">Text comment</label>
-        <textarea class="form-control" id="comment" rows="3" placeholder="Text comment"></textarea>
+        <textarea class="form-control" name="comment" id="comment" rows="3" placeholder="Text comment"></textarea>
     </div>
     <div class="d-grid gap-1">
         <button class="btn btn-primary" type="submit">Leave a comment</button>
